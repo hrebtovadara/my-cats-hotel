9c792d7f01431221659401660b2e3b63825afd86
dasha <hrebtovadara@gmail.com>
2020-07-15 15:17:44 +0300

    js code for filter

--- source/js/index.js
+++ source/js/index.js
@@ -30,3 +30,43 @@ for (let i = 0; i < roomsInputArr.length; i++) {
     roomsBlockArr[i].classList.remove("visually-hidden");
   })
 }
+
+let filterButton = document.querySelector(".catalog-filters__button");
+let filterBlur = document.querySelector(".catalog-filters__blur");
+let filterMenu = document.querySelector(".filter");
+let filterCloseButton = document.querySelector(".filter__close");
+
+
+window.addEventListener('resize', function() {
+  let width = document.documentElement.clientWidth;
+
+  if (document.documentElement.clientWidth < 1366) {
+    filterButton.classList.remove("visually-hidden");
+    filterBlur.classList.add("visually-hidden");
+    filterMenu.classList.add("visually-hidden");
+
+    filterButton.onclick = function () {
+      filterButton.classList.toggle("visually-hidden");
+      filterBlur.classList.toggle("visually-hidden");
+      filterMenu.classList.toggle("visually-hidden");
+    }
+  
+    filterCloseButton.onclick = function () {
+      filterButton.classList.toggle("visually-hidden");
+      filterBlur.classList.toggle("visually-hidden");
+      filterMenu.classList.toggle("visually-hidden");
+    }
+  } else {
+    filterButton.classList.add("visually-hidden");
+    filterBlur.classList.add("visually-hidden");
+    filterMenu.classList.remove("visually-hidden");
+  }
+});
+
+// let sortButton = document.querySelector(".sort__button");
+// let sortContainer = document.querySelector(".sort__container");
+
+// sortButton.onclick = function() {
+//   sortButton.classList.toggle("visually-hidden");
+//   sortContainer.classList.toggle("visually-hidden");
+// }
